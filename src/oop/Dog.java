082c39942465650073ce37cb1d16a3296722bc26
odessabear@gmail.com <odessabear@gmail.com>
2018-09-18 19:30:28 +0300

    start of the oop lesson

---
+++ src/oop/Dog.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Dog {
+    private int paws = 4;
+    private int tale = 1;
+    private String name;
+    private String breed;
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
