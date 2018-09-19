d1e23752956a1b89c1cee694213877f209679d5c
odessabear@gmail.com <odessabear@gmail.com>
2018-09-19 19:12:06 +0300

    start of the oop (classes and static word) lesson

--- src/oop/Dog.java
+++ src/oop/Dog.java
@@ -1,12 +1,95 @@
 package oop;
 
 public class Dog {
+
+    private static int dogsCount;
+
     private int paws = 4;
     private int tale = 1;
     private String name;
     private String breed;
+    private String size;
+
+    public Dog(){
+        dogsCount++;
+        System.out.println("Dog's count is " + dogsCount);
+    }
+
+    public static int getDogsCount(){
+        return dogsCount;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if (size.equalsIgnoreCase("big")||
+                size.equalsIgnoreCase("average")||
+                size.equalsIgnoreCase("small")) {
+            this.size = size;
+        }else
+            System.out.println("Size should be one of these : Big , Average or Small!");
+    }
 
     public void setName(String name){
         this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setPaws(int paws){
+        if (paws == 4) {
+            this.paws = paws;
+        }else {
+            System.out.println("User tried to assign " + paws + " paws for a dog");
+            System.out.println("Correct number is 4!");
+        }
+    }
+
+    public int getPaws(){
+        return paws;
+    }
+
+    public int getTale() {
+
+        return tale;
+    }
+
+    public void setTale(int tale) {
+        if (tale == 1){
+        this.tale = tale;}
+        else {
+            this.tale = 1;
+            System.out.println("User tried to assign " + tale + " tails for dog");
+            System.out.println("Correct number is 1 !!!");
+        }
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void bark(){
+        if (size.equalsIgnoreCase("Big")){
+            System.out.println("woof-woof");
+        }else if (size.equalsIgnoreCase("Average")){
+            System.out.println("raf-raf");
+        }else {
+            System.out.println("tiaf-tiaf");
+        }
+
+    }
+
+    public void bite(){
+        if (dogsCount>2){
+            System.out.println("Dogs are biting you");
+        }else bark();
     }
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -2,7 +2,33 @@ package oop;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
+
+        System.out.println("Dog's count "+ Dog.getDogsCount());
+
+        Dog lab = new Dog();
+        lab.setPaws(4);
+        lab.setTale(1);
+        lab.setName("Charley");
+        lab.setBreed("Labrador");
+        lab.setSize("Average");
+        lab.bark();
+
+
+        Dog sheppard = new Dog();
+        sheppard.setPaws(4);
+        sheppard.setTale(1);
+        sheppard.setName("Mike");
+        sheppard.setBreed("Sheppard");
+        sheppard.setSize("Big");
+        sheppard.bark();
+
+        Dog doberman = new Dog();
+        doberman.setPaws(4);
+        doberman.setTale(1);
+        doberman.setBreed("Doberman");
+        doberman.setName("Jack");
+        doberman.bite();
+
 
     }
 }
