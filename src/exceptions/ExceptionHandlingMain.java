3792a538a31142a95a80df09b6db85e8c3fe0c55
amikhailov <bearodessa@gmail.com>
2018-10-17 23:46:03 +0300

    exceptions lessons

--- src/exceptions/ExceptionHandlingMain.java
+++ src/exceptions/ExceptionHandlingMain.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,23 +10,40 @@ public class ExceptionHandlingMain {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try {
-            System.out.println("Please enter numerator");
-            int numerator = scanner.nextInt();
-            System.out.println("Please enter denominator");
-            int denominator = scanner.nextInt();
-            System.out.println(divide(numerator,denominator));
-        } catch (ArithmeticException e) {
-            System.out.println("Exception :" +e);
-            scanner.nextLine();
-            System.out.println("Only non-zero parameters allowed");
-        }catch (InputMismatchException e){
+        PrintWriter writer = null;
+        boolean continueLoop = true;
+        do {
+            try {
+                System.out.println("Please enter numerator");
+                int numerator = scanner.nextInt();
+                System.out.println("Please enter denominator");
+                int denominator = scanner.nextInt();
+               // System.out.println(divide(numerator, denominator));
+                writer = new PrintWriter(new FileWriter("out.txt"));
+                writer.println("Result " + divide(numerator, denominator));
 
-        }
+                continueLoop = false;
+            } catch (ArithmeticException | InputMismatchException e) {
+                System.out.println("Exception :" + e);
+                scanner.nextLine();
+                System.out.println("Only integer non-zero parameters allowed");
+            } catch (IOException e) {
+                System.out.println("Unable to open file");
+                e.printStackTrace();
+            } finally {
+                System.out.println("Finally block called");
+                writer.close();
+            }
+            System.out.println("Try catch block finished");
+        } while (continueLoop);
 
     }
 
     private static int divide(int numerator, int denominator) {
         return numerator / denominator;
     }
+
+//    private static void saveToFile(int res) throws IOException {
+//
+//    }
 }
