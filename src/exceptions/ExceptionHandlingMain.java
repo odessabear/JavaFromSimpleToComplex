ec69dc1565375f36d7b6672003efaa6792cfdd5c
odessabear@gmail.com <odessabear@gmail.com>
2018-10-19 19:14:46 +0300

    exceptions last lessons start

--- src/exceptions/ExceptionHandlingMain.java
+++ src/exceptions/ExceptionHandlingMain.java
@@ -1,27 +1,35 @@
 package exceptions;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionHandlingMain {
 
     public static void main(String[] args) {
+        try {
+            doEverything();
+        } catch (InvalidInputParamException e) {
+            System.out.println("InvalidInputParamException");
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void doEverything() {
         Scanner scanner = new Scanner(System.in);
-        PrintWriter writer = null;
+
         boolean continueLoop = true;
         do {
-            try {
+            try (PrintWriter writer = new PrintWriter(new FileWriter("out.txt")); BufferedReader reader = new BufferedReader(new FileReader("1234"))) {
                 System.out.println("Please enter numerator");
                 int numerator = scanner.nextInt();
                 System.out.println("Please enter denominator");
                 int denominator = scanner.nextInt();
-               // System.out.println(divide(numerator, denominator));
-                writer = new PrintWriter(new FileWriter("out.txt"));
+//                // System.out.println(divide(numerator, denominator));
+//                int[] intArray = new int[1];
+//                int i = intArray[2];
                 writer.println("Result " + divide(numerator, denominator));
-
                 continueLoop = false;
             } catch (ArithmeticException | InputMismatchException e) {
                 System.out.println("Exception :" + e);
@@ -30,16 +38,17 @@ public class ExceptionHandlingMain {
             } catch (IOException e) {
                 System.out.println("Unable to open file");
                 e.printStackTrace();
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("All exceptions here");
+                throw new InvalidInputParamException("Index out of bound.thrown in doEverything" + e);
             } finally {
                 System.out.println("Finally block called");
-                writer.close();
             }
             System.out.println("Try catch block finished");
         } while (continueLoop);
-
     }
 
-    private static int divide(int numerator, int denominator) {
+    private static int divide(int numerator, int denominator) throws ArithmeticException, NullPointerException {
         return numerator / denominator;
     }
 
---
+++ src/exceptions/SwelfClosable.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+import java.io.Closeable;
+import java.io.FileNotFoundException;
+
+public interface SwelfClosable extends Closeable {
+@Override
+    void close() throws FileNotFoundException;
+}
