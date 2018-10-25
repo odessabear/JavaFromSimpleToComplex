8f0f66aa0b255cf325dc782d1c26433090a05b4e
odessabear@gmail.com <odessabear@gmail.com>
2018-10-25 18:59:39 +0300

    ArrayList lessons part2(beginning)

--- src/collections/lists/ListRunner.java
+++ src/collections/lists/ListRunner.java
@@ -1,10 +1,51 @@
 package collections.lists;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class ListRunner {
 
-    public static void main(String[] args) {
+    private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        ToDoList list = new ToDoList();
+        printOut();
+        int param = scanner.nextInt();
+        while (param != 0) {
+            switch (param) {
+                case 1:
+                    System.out.println("Please enter a task to add");
+                    scanner.nextLine();
+                    String task1 = scanner.nextLine();
+                    list.addToList(task1);
+                    System.out.println("Please choose an action by typing [0-6]");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    param = 0;
+
+
+            }
+        }
+    }
+
+    private static void printOut() {
+        System.out.println("Please choose an action.Press :\n"
+                + "1 to add a new item into ToDoList\n"
+                + "2 to print out the list\n"
+                + "3 to update an existing item\n"
+                + "4 to remove an item from the list\n"
+                + "5 to get task priority or number in the list\n"
+                + "6 to add a new item at specific position\n"
+                + "pres 0 for exit\n\n"
+                + "AFTER CHOOSING AN OPTION PLEASE PRESS ENTER");
     }
 }
