abbe99ec4667891caadc2f405ec21cc5b043f265
odessabear@gmail.com <odessabear@gmail.com>
2018-11-08 21:13:08 +0200

    Comparable and Comparator lessons

--- src/collections/collectionsClass/CollectionsRunner.java
+++ src/collections/collectionsClass/CollectionsRunner.java
@@ -1,12 +1,34 @@
 package collections.collectionsClass;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CollectionsRunner {
     public static void main(String[] args) {
+        List<Card> deckOfCards = new ArrayList<>();
+        for (Card.Face face : Card.Face.values()) {
+            for (Card.Suit suit : Card.Suit.values()) {
+                deckOfCards.add(new Card(suit, face));
+            }
+        }
 
+//        System.out.println("Original deck of cards");
+//        for (int i = 0; i < deckOfCards.size(); i++) {
+//            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+//        }
+        Collections.shuffle(deckOfCards);
 
+        System.out.println("\n\nCards After shuffle");
+        System.out.println();
+        for (int i = 0; i < deckOfCards.size(); i++) {
+            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+        }
+        System.out.println();
+        Collections.sort(deckOfCards, new CardComparator());
+        System.out.println("Cards after sorting");
+        System.out.println();
+        for (int i = 0; i < deckOfCards.size(); i++) {
+            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+        }
     }
 
     public static class Card implements Comparable<Card> {
@@ -37,6 +59,11 @@ public class CollectionsRunner {
         }
 
         @Override
+        public String toString() {
+            return face + " of " + suit;
+        }
+
+        @Override
         public int compareTo(Card card) {
             Face[] values = Face.values();
             List<Face> faces = Arrays.asList(values);
@@ -45,10 +72,27 @@ public class CollectionsRunner {
                 return -1;
             } else if (faces.indexOf(this.face) > faces.indexOf(card.getFace())) {
                 return +1;
-            }else if (faces.indexOf(this.face) == faces.indexOf(card.getFace())){
+            } else if (faces.indexOf(this.face) == faces.indexOf(card.getFace())) {
                 return String.valueOf(suit).compareTo(String.valueOf(card.getSuit()));
             }
             return 0;
         }
     }
+
+    public static class CardComparator implements Comparator<Card> {
+        List<Card.Face> faces = Arrays.asList(Card.Face.values());
+
+        @Override
+        public int compare(Card card1, Card card2) {
+            if (faces.indexOf(card1.getFace()) < faces.indexOf(card2.getFace())) {
+                return 1;
+            } else if (faces.indexOf(card1.getFace()) > faces.indexOf(card2.getFace())) {
+                return -1;
+            } else if (faces.indexOf(card1.getFace()) == faces.indexOf(card2.getFace())) {
+                return String.valueOf(card1.getSuit()).compareTo(String.valueOf(card2.getSuit()));
+            }
+            return 0;
+        }
+    }
+
 }
