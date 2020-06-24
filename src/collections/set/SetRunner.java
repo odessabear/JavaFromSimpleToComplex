0d9f88dd15e18870d0a786bdb3891c70d04561fe
odessabear <bearodessa@gmail.com>
2020-06-24 20:26:37 +0300

    collections in the process

--- src/collections/set/SetRunner.java
+++ src/collections/set/SetRunner.java
@@ -6,21 +6,66 @@ public class SetRunner {
 
     public static void main(String[] args) {
 
-        String[] cars = {"Mini", "Mercedes-Benz", "Audi", "VW", "Smart", "Toyota", "Porsche"};
-        String[] otherCars = {"Audi", "Ford", "GMC", "Toyota", "Chevrolet"};
+//        String[] cars = {"Mini", "Mercedes-Benz", "Audi", "VW", "Smart", "Toyota", "Porsche"};
+//        String[] otherCars = {"Audi", "Ford", "GMC", "Toyota", "Chevrolet"};
+//
+//        Set<String> carSet = new TreeSet<>(Arrays.asList(cars));
+//
+//        Set<String> otherCarSet = new TreeSet<>(Arrays.asList(otherCars));
+//
+//        Set<String> uniqueCars = new TreeSet<>(carSet);
+//        uniqueCars.addAll(otherCarSet);
+//
+//        print(uniqueCars);
 
-        Set<String> carSet = new TreeSet<>(Arrays.asList(cars));
+        Set<Car> sixCars = new HashSet<>();
+        sixCars.add(new Car("VW", "Golf", 45));
+        sixCars.add(new Car("Audi", "A3", 60));
+        sixCars.add(new Car("VW", "Polo", 35));
+        sixCars.add(new Car("BMW", "Z4", 120));
+        sixCars.add(new Car("BMW", "404i", 200));
 
-        Set<String> otherCarSet = new TreeSet<>(Arrays.asList(otherCars));
+        Set<Car> europaCars = new HashSet<>();
+        europaCars.add(new Car("Toyota", "Auris", 40));
+        europaCars.add(new Car("Reno", "Clio", 30));
+        europaCars.add(new Car("Reno", "Megan", 50));
+        europaCars.add(new Car("VW", "Golf", 45));
+        europaCars.add(new Car("VW", "Polo", 35));
 
-        Set<String> uniqueCars = new TreeSet<>(carSet);
-        uniqueCars.addAll(otherCarSet);
+        NavigableSet<Car> uniqCars = new TreeSet<>(sixCars);
+        uniqCars.addAll(europaCars);
+//
+//        print(uniqCars);
+//        System.out.println();
+//
+//        sixCars.removeAll(europaCars);
+//        print(sixCars);
+//        System.out.println();
+//
+//        europaCars.removeAll(sixCars);
+//        print(europaCars);
 
-        print(uniqueCars);
+//        sixCars.retainAll(europaCars);
+//        uniqCars.removeAll(sixCars);
 
+        SortedSet<Car> cars = uniqCars.subSet(new Car("Toyota", "Auris", 40), true, new Car("Audi", "A3", 60), true);
+
+
+        print(uniqCars);
+
+        System.out.println("Higher");
+        System.out.println(uniqCars.higher(new Car("Toyota", "Auris", 40)));
+
+        System.out.println();
+        System.out.println("Lower");
+        System.out.println(uniqCars.lower(new Car("Toyota", "Auris", 40)));
     }
 
-    private static void print(Set<String> cars) {
-        System.out.println(cars);
+    private static void print(Set<Car> cars) {
+        System.out.printf("%-20s %-20s %-20s \n", "Car brand", "Model", "Price per day");
+
+        for (Car car : cars) {
+            System.out.printf("%-20s %-20s %-20s \n", car.getCarBrand(), car.getModel(), car.getPricePerDay());
+        }
     }
 }
